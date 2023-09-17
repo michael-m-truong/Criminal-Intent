b464e34a2f1f4928c00bfcbad232b409b4372d91
Michael Truong <truong.minh.michael@gmail.com>
2023-09-16 20:13:18 -0700

    finished ch 11

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,13 @@ class CrimeHolder(
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+
 
     }
 }
